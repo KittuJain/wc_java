0550c7e5f9956dbeaedca314b65e11fac4774655
Kittu Jain <cskratijain@gmail.com>
2015-02-17 11:27:05 +0530

    getLines functionality completed

---
+++ Wc.java
@@ -0,0 +1,10 @@
+public class Wc {
+	public int getLines(String text){
+		int numOfLines = 0;
+		for (int i = 0; i < text.length(); i++ ) {
+			if(text.charAt(i) == '\n')
+				numOfLines++;
+		}
+		return numOfLines;
+	}
+}
