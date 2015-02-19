786e659dbca882ba657428cc77caf43051cbfb4c
Kittu Jain <cskratijain@gmail.com>
2015-02-19 15:36:18 +0530

    version 2 completed for command line fileInput

--- Wc.java
+++ Wc.java
@@ -1,8 +1,17 @@
-// import java.io.*;
-// public class Wc {
+import java.io.*;
+public class Wc {
 
-// 	public static void main(String[] args) throws IOException{
-// 		WcMain wcMain = new WcMain(args[1]);
-// 		System.out.println(wcMain.fileHandler());
-// 	}
-// }
+	public static void main(String[] args) throws IOException{
+		String fileName = args[0], option = null;
+		if(args.length==2){
+			option = args[1];
+			if(fileName.charAt(0) == '-'){
+				String file = fileName;
+				fileName = option;
+				option = file;
+			}
+		}
+		WcMain wcMain = new WcMain(fileName, option);
+		System.out.println(wcMain.fileHandler());
+	} 
+}
--- WcMain.java
+++ WcMain.java
@@ -1,30 +1,32 @@
-// import java.io.*;
-// public class WcMain {
-// 	String fileName;
+import java.io.*;
+public class WcMain {
+	String fileName, option = null;
 	
-// 	public WcMain(String fileName){
-// 		this.fileName = fileName;
-// 	}
+	public WcMain(String fileName){
+		this.fileName = fileName;
+	}
+	public WcMain(String fileName, String option){		
+		this.fileName = fileName;
+		this.option = option;
+	}
 	
-// 	public String fileHandler() throws IOException{
-// 		File file = new File(fileName);
-// 		int length = (int)file.length();
+	public String fileHandler() throws IOException{
+		File file = new File(fileName);
+		int length = (int)file.length();
 		
-// 		char[] cbuf = new char[length];
-// 		FileReader fr = null;
+		char[] cbuf = new char[length];
+		FileReader fr = null;
 
-// 		try{
-// 			fr = new FileReader(file);
-// 		}
-// 		catch(Exception e){
+		try{
+			fr = new FileReader(file);
+		}
+		catch(Exception e){}
 
-// 		}
-
-// 		BufferedReader br = new BufferedReader(fr);
-// 		br.read(cbuf, 0, length);
-// 		String text = new String(cbuf);
-// 		System.out.println(cbuf);
-// 		WcLib wc = new WcLib(text);
-// 		return wc.getWordCount();
-// 	}
-// }
+		BufferedReader br = new BufferedReader(fr);
+		br.read(cbuf, 0, length);
+		String text = new String(cbuf);
+		WcLib wc = new WcLib(text,option);
+		wc.getWordCount();
+		return wc.toString();
+	}
+}
